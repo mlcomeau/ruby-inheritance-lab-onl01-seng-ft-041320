7e249c273d93dc1f5dcac6529bf6be5f766670cb
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 19:33:22 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,9 @@
-class User 
+class User
   attr_reader :first_name, :last_name
 
-  def initialize 
+  def initialize
     @first_name = first_name
     @last_name = last_name
   end
-  
-end
 
+end
