f90a42afcbf0ee5f9961e44e2a4db3be6c351989
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 19:43:17 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+  def initialize
+    @knowledge = []
+  end
+
+  def learn (new_stuff)
+    self.knowledge << new_stuff
+  end
+
+  def knowledge
+    @knowledge
+  end
+
+
 
 end
