1787668998b9e375f9f762caf89c278946a4be4a
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 19:39:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,18 @@
 require_relative "./user.rb"
-
+attr_accessor :knowledge 
 class Student < User
+  def initialize 
+    @knowledge = []
+  end
+
+  def learn (new_stuff)
+    self.knowledge << new_stuff 
+  end
+
+  def knowledge 
+    @knowledge 
+  end 
+  
+    
 
 end
